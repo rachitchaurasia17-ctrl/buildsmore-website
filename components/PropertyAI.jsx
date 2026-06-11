6b8e05d0b8d2a30862189248046316f3faa7ae41
rachitchaurasia17-ctrl <rachitchaurasia17@gmail.com>
2026-06-11 22:39:29 +0530

    Fix: replace all template addresses with client address

--- components/PropertyAI.jsx
+++ components/PropertyAI.jsx
@@ -11,7 +11,7 @@ const SYSTEM_PROMPT = `You are Property AI, the digital concierge for BuildsMore
 
 Firm facts:
 - Firm: BuildsMore
-- Office: SCO 1, 2nd Floor, Sector 79, Sahibzada Ajit Singh Nagar, Punjab 140308
+- Office: Commercial Hub, Phase 11, Mohali, Punjab
 - Phone: +91-00000-00000
 - GMADA empanelled, RERA registered (PB-RERA)
 - Practice: Residential, Commercial, Land/Plots, Investment Advisory, NRI Services, Title & Due Diligence
